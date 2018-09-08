284f2b1b56368f93ee6da87604a7e4e1d031d7d9
marcelo-oktana <marcelom@oktana.io>
2018-09-07 22:00:11 -0300

    Serve static files

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
+app.use('/static', express.static('static'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/templates/index.html'));
 });
